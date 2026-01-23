121635f71480cb5a6b7dd2d5b387bff2940abba8
Bala-A87 <bala8702@gmail.com>
2026-01-23 20:46:57 +0530

    Add basic files

---
+++ src/script.js
@@ -0,0 +1,18 @@
+const messages = ['Please say yes!', 'Aww please....!!'];
+messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector(".no-button");
+    const yesButton = document.querySelector(".yes-button");
+
+    // noButton.textContent = messages[messageIndex];
+    // messageIndex += 1;
+    // messageIndex %= messages.length;
+
+    const currSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
